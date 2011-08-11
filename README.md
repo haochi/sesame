43c93cc0fe4905bcc76fa5e340d4a48795e6e700
Haochi Chen <haochi.chen@gmail.com>
2011-08-11 00:27:45 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Sesame
+
+A small helper that helps you organize your JavaScript code by detecting some sort of ID (e.g. the value of a variable or id of an element).
+
+# Example
+
+    <body id="stuff"></body>
+    <script src="sesame.js"></script>
+    <script>
+    sesame.add("stuff", function(){
+      document.title = "lol";
+    });
+    sesame.add(["stuff"], function(){
+      location.hash = (new Date).getTime();
+    });
+
+    // chaing
+    sesame.after(function(){
+      alert(0);
+    }).run(document.body.id);
+    </script>
+
+# Methods
+
+* `add(detections, function)`: `detections` can be a string or an array of strings, which will be detected by the `run` method
+* `before(function)`: this gets executed every time before all other functions that sesame handles get executed
+* `after(function)`: similar to `before`, but after everything else is executed
+* `run(detection)`: `detection` is expected to be a string of whatever you want to detect. this will trigger all the matching functions sesame handles to execute.
---
+++ sesame.js
@@ -0,0 +1,53 @@
+(function(){
+  function sesame(){
+    this.detections = {};
+  }
+
+  sesame.prototype = {
+    before: function(fn){
+      this.before = fn;
+      return this;
+    },
+    add: function(detections, fn){
+      if(detections instanceof Array){
+        for(var i=0, l=detections.length; i<l; i++){
+          this._add(detections[i], fn);
+        }
+      }else{
+        this._add(detections, fn);
+      }
+      return this;
+    },
+    after: function(fn){
+      this.after = fn;
+      return this;
+    },
+    "new": function(){
+      return new sesame();
+    },
+    run: function(detection){
+      var noop = function(){};
+      (this.before || noop)();
+      if(detection != null){
+        if(detection in this.detections){
+          var detections = this.detections[detection];
+          for(var i=0, l=detections.length; i<l; i++){
+            detections[i]();
+          }
+        }
+      }else{
+        throw "Nothing to detect.";
+      }
+      (this.after || noop)();
+      return this;
+    },
+    _add: function(detection, fn){
+      if(!(detection in this.detections)){
+        this.detections[detection] = [];
+      }
+      this.detections[detection].push(fn);
+    }
+  };
+
+  window.sesame = new sesame();
+})();
